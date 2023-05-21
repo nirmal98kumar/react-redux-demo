821cef0a4acf18bfdf08fc9d35fe748e7f0ce627
nirmal98kumar <nirmal98kumar@gmail.com>
2023-05-21 13:02:33 +0530

    devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
-import { createStore } from "redux";
 import rootReducer from "./rootReducer";
-
-const store = createStore(rootReducer)
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+const store = createStore(rootReducer,composeWithDevTools(
+    applyMiddleware()
+  ))
 
 export default store
